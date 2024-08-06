0fe0f208fa5787c21d685153bd755dfabccfc7a7
lukas.smicys <lukas.smicys@ext.fmc-ag.com>
2024-08-06 19:21:44 +0300

    Finds something.

--- main.py
+++ main.py
@@ -2,92 +2,82 @@ import argparse
 from pathlib import Path
 import re
 
-pattern = "Simple"
 
-current = Path(__file__)  # C:\git_repos\qdev\main.py
-current = Path.home()  # C:\Users\lukas.smicys
-current = Path.cwd()  # C:\git_repos\qdev
-current = Path.cwd().parent  # C:\git_repos
-current = Path.cwd().parents[1]  # C:\
-current = Path()  # .
-current = Path(".")  # .
-current = Path("C:\\Users\\lukas.smicys\\Desktop\\My first py")  # **\*.py
-
-print(f"# {current}")
-
-current.iterdir()
-
-
-# for file in current.rglob("*"):
-#     print(file)
-
-
-def find_pattern_by_line(file, pattern, case_sensitive=True):
-    with open(file, "r", encoding="UTF-8") as f:
-        lines = f.readlines()
-        for index, line in enumerate(lines):
-            if case_sensitive:
-                if pattern in line:
-                    print(index, line)
-            else:
-                if pattern.lower() in line.lower():
-                    print(index, line)
+def find_pattern_by_line(file: Path, pattern, ignore_case=False):
+    try:
+        lines = file.read_text(encoding="UTF-8").splitlines()
+    except UnicodeDecodeError:
+        return
+    for index, line in enumerate(lines):
+        if not ignore_case:
+            match = re.search(pattern, line)
+            if match:
+                print(index, line)
+        else:
+            match = re.search(pattern.lower(), line.lower())
+            if match:
+                print(index, line)
 
 
 path_pattern = "*.txt"
 current_path = Path()
 pattern = "Simple"
+recursive = True
+ignore_case = True
 
-for file in current_path.rglob(
-        path_pattern):  # Looking for pattern in files level
-    print(file)
-    find_pattern_by_line(file, pattern)
+search_func = current_path.glob("*")
+if recursive:
+    search_func = current_path.rglob("*")
+
+if current_path.is_dir():
+    for item in search_func:
+        if item.is_file():
+            find_pattern_by_line(item, pattern, ignore_case)
+
+elif current_path.is_file():
+    find_pattern_by_line(current_path, pattern, ignore_case)
+else:
+    print(f"We dont know what it is, better dont touch.")
 
 
 def search_for_pattern(arg: argparse.Namespace):
-    current_path: Path = arg.file_path
+    current_path = arg.file_path
     pattern = arg.pattern
+    count = 0
+
+    search_func = current_path.glob("*")
+    if args.recursive:
+        search_func = current_path.rglob("*")
+
+    if args.extended_re:
+        pattern = args.extended_re
 
     if current_path.is_dir():
-        for item in current_path.iterdir():
+        for item in search_func:
             if item.is_file():
-                find_pattern_by_line(item, pattern)
-            else:
-                print(f"We dont go deeper try adding -r flag to cmd")
+                find_pattern_by_line(item, pattern, arg.ignore_case)
 
     elif current_path.is_file():
-        find_pattern_by_line(current_path, pattern)
+        find_pattern_by_line(current_path, pattern, arg.ignore_case)
 
     else:
         print(f"We dont know what it is, better dont touch.")
         return
 
-    if arg.ignore_case:
-        print("ignore case")
-
-    if arg.recursive:
-        for file in current_path.rglob("*"):
-            find_pattern_by_line(file, pattern)
-
-
-# Path can be dir. Then we search in dir, how to apply case_sensitive.
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find pattern in files")
     parser.add_argument('file_path', type=Path, )  # positional argument
     parser.add_argument('pattern', type=str)  # positional argument
-    # parser.add_argument('-E', '--extended_regex')
-    # parser.add_argument('-A', '--after_context')
-    # parser.add_argument('-B', '--before_context')
-    # parser.add_argument('-C', '--context')
-    parser.add_argument('-i', '--ignore_case',
-                        action='store_true')  # on/off flag
-    # parser.add_argument('-c', '--count',
-    #                     action='store_true')  # on/off flag
-    # parser.add_argument('-n', '--line_number',
-    #                     action='store_true')  # on/off flag
-    parser.add_argument('-r', '--recursive',
-                        action='store_true')  # on/off flag
+    # on/off flag action='store_true'
+    parser.add_argument('-E', '--extended_re', type=str)
+    parser.add_argument('-A', '--after_context', type=int)
+    parser.add_argument('-B', '--before_context', type=int)
+    parser.add_argument('-C', '--context', type=int)
+    parser.add_argument('-i', '--ignore_case', action='store_true')
+    parser.add_argument('-c', '--count', action='store_true')
+    parser.add_argument('-n', '--line_number', action='store_true')
+    parser.add_argument('-r', '--recursive', action='store_true')
 
     args = parser.parse_args()
     print(args.file_path, args.pattern)
