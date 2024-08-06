b19edfeff6130a8317a13ab0e88832c138977b91
lukas.smicys <lukas.smicys@ext.fmc-ag.com>
2024-08-06 14:25:17 +0300

    Mapping my head paths with Path lib.

--- main.py
+++ main.py
@@ -2,34 +2,75 @@ import argparse
 from pathlib import Path
 import re
 
-# current_file = Path("C:\git_repos\qdev\config\input.txt")
-# lines = current_file.read_text(encoding="UTF-8").splitlines()
-# for index, line in enumerate(lines):
-#     print(index, line)
+pattern = "Simple"
 
 current = Path(__file__)  # C:\git_repos\qdev\main.py
 current = Path.home()  # C:\Users\lukas.smicys
 current = Path.cwd()  # C:\git_repos\qdev
 current = Path.cwd().parent  # C:\git_repos
 current = Path.cwd().parents[1]  # C:\
+current = Path()  # .
+current = Path(".")  # .
+current = Path("C:\\Users\\lukas.smicys\\Desktop\\My first py")  # **\*.py
+
 print(f"# {current}")
 
+current.iterdir()
 
-def search_for_pattern(arg: argparse.Namespace):
-    current_path = arg.file_path
+
+# for file in current.rglob("*"):
+#     print(file)
+
+
+def find_pattern_by_line(file, pattern, case_sensitive=True):
+    with open(file, "r", encoding="UTF-8") as f:
+        lines = f.readlines()
+        for index, line in enumerate(lines):
+            if case_sensitive:
+                if pattern in line:
+                    print(index, line)
+            else:
+                if pattern.lower() in line.lower():
+                    print(index, line)
+
+
+path_pattern = "*.txt"
+current_path = Path()
+pattern = "Simple"
+
+for file in current_path.rglob(
+        path_pattern):  # Looking for pattern in files level
+    print(file)
+    find_pattern_by_line(file, pattern)
+
+
+def search_for_pattern(arg: argparse.Namespace):  # why we can pass in multiple?
+    current_path: Path = arg.file_path  # How knows, is it because parse_args() later?
     pattern = arg.pattern
 
-    with open(current_path, mode="r", encoding="UTF-8") as file:
-        for index, line in enumerate(file):
-            if pattern in line:
-                print(index, line)
+    if current_path.is_dir():
+        for item in current_path.iterdir():
+            if item.is_file():
+                find_pattern_by_line(item, pattern)
+            else:
+                f"We dont go deeper try adding -r flag to cmd"
+
+    elif current_path.is_file():
+        find_pattern_by_line(current_path, pattern)
+
+    else:
+        f"We dont know what it is, better dont touch."
+        return
 
     if arg.ignore_case:
         print("ignore case")
 
     if arg.recursive:
-        print("recursive")
+        for file in current_path.rglob("*"):
+            find_pattern_by_line(file, pattern)
 
+
+# Path can be dir. Then we search in dir, how to apply case_sensitive.
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find pattern in files")
