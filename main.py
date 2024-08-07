df5f624ab838c280b76a022d662a50313c564ab8
lukas.smicys <lukas.smicys@ext.fmc-ag.com>
2024-08-07 17:34:38 +0300

    Added regex flag to find_pattern_by_line method.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import re
 from typing import Generator
 
 
-def find_pattern_by_line_regex(file: Path, pattern, ignore_case=False,
-                               line_nr=False):
+def find_pattern_by_line(file: Path, pattern, ignore_case=False,
+                         line_nr=False, extend_re=False):
     count = 0
     try:
         lines = file.read_text(encoding="UTF-8").splitlines()
@@ -14,77 +14,78 @@ def find_pattern_by_line_regex(file: Path, pattern, ignore_case=False,
         return count
 
     for index, line in enumerate(lines):
-        match = re.search(pattern, line)  # "<re.Match object; span=(11, 17), match='simple'>"
+        match = pattern in line
 
         if ignore_case:
-            match = re.search(pattern, line, flags=re.IGNORECASE)
+            match = pattern.lower() in line.lower()
+
+        if extend_re:
+            # "<re.Match object; span=(11, 17), match='simple'>"
+            match = re.search(pattern, line)
+
+            if ignore_case:
+                match = re.search(pattern, line, flags=re.IGNORECASE)
 
         if match:
             count += 1
-            print(index, line if line_nr else line)
+            print((index, line) if line_nr else line)
 
     return count
 
 
-def find_pattern_by_line(file: Path, pattern, ignore_case=False,
-                         line_nr=False):
-    count = 0
-    try:
-        lines = file.read_text(encoding="UTF-8").splitlines()
-    except UnicodeDecodeError:
-        return count
+# def find_pattern_by_line(file: Path, pattern, ignore_case=False,
+#                          line_nr=False):
+#     count = 0
+#     try:
+#         lines = file.read_text(encoding="UTF-8").splitlines()
+#     except UnicodeDecodeError:
+#         return count
+#
+#     for index, line in enumerate(lines):
+#
+#         if ignore_case:
+#             pattern = pattern.lower()
+#             line = line.lower()
+#
+#         if pattern in line:
+#             count += 1
+#             print(index, line if line_nr else line)
+#
+#     return count
 
-    for index, line in enumerate(lines):
 
-        if ignore_case:
-            pattern = pattern.lower()
-            line = line.lower()
+current_path = Path(".")
+pattern = "simple"  # "^def\s" "simple"
+recursive = True
+ignore_case = True
+line_number = False
+regex = True
+count_arg = True
 
-        if pattern in line:
-            count += 1
-            print(index, line if line_nr else line)
+count = 0
 
-    return count
+search_level = current_path.glob("*")
+if recursive:
+    search_level = current_path.rglob("*")
 
+if ignore_case:
+    pattern = pattern.lower()
 
-# current_path = Path(".")
-# pattern = "^def\s"  # "^def\s" "simple"
-# recursive = False
-# ignore_case = False
-# line_number = False
-# extended_re = True
-# count_arg = True
-#
-# count = 0
-#
-# search_level = current_path.glob("*")
-# if recursive:
-#     search_level = current_path.rglob("*")
-#
-# if ignore_case:
-#     pattern = pattern.lower()
-#
-# if current_path.is_dir():
-#     for item in search_level:
-#         if item.is_file() and extended_re:
-#             count += find_pattern_by_line_re(item, pattern, ignore_case,
-#                                              line_number)
-#         elif item.is_file() and not extended_re:
-#             count += find_pattern_by_line(item, pattern, ignore_case,
-#                                           line_number)
-#
-# elif current_path.is_file() and extended_re:
-#     count += find_pattern_by_line_re(current_path, pattern, ignore_case,
-#                                      line_number)
-# elif current_path.is_file() and not extended_re:
-#     count += find_pattern_by_line(current_path, pattern, ignore_case,
-#                                   line_number)
-#
-# else:
-#     print(f"We dont know what it is, better dont touch.")
-#
-# if count_arg:
-#     print(f"Number of rows where pattern repeats is {count}")
+if current_path.is_dir():
+    for item in search_level:
+        if item.is_file():
+            count += find_pattern_by_line(item, pattern, ignore_case,
+                                          line_number, regex)
+
+elif current_path.is_file():
+    count += find_pattern_by_line(current_path, pattern, ignore_case,
+                                  line_number, regex)
+
+else:
+    print(f"We dont know what it is, better dont touch.")
+
+if count_arg:
+    print(f"Number of rows where pattern repeats is {count}")
 
 
 def search_for_pattern(args: argparse.Namespace):
@@ -92,15 +93,15 @@ def search_for_pattern(args: argparse.Namespace):
     pattern = args.pattern
     count = 0
 
-# TODO: this bellow
+    # TODO: this bellow
     if not current_path.exists():
         raise Exception("Bad dir")
 
-# TODO: class with unpacking args.Namespaces and add types.
-# TODO: when finished actual operation exit -> return
-# TODO: add more defs and a class.
-# TODO: args.before.extend(lines[max(index-3, 0):index])
-# TODO: args.before.extend(lines[max(index+3, len(lines):index])
+    # TODO: class with unpacking args.Namespaces and add types.
+    # TODO: when finished actual operation exit -> return
+    # TODO: add more defs and a class.
+    # TODO: args.before.extend(lines[max(index-3, 0):index])
+    # TODO: args.before.extend(lines[max(index+3, len(lines):index])
 
     searched_files = current_path.glob("*")
     if args.recursive:
@@ -108,20 +109,16 @@ def search_for_pattern(args: argparse.Namespace):
 
     if current_path.is_dir():
         for item in searched_files:
-            if item.is_file() and args.extended_re:
-                count += find_pattern_by_line_regex(item, pattern,
-                                                    args.ignore_case,
-                                                    args.line_number)
-            elif item.is_file() and not args.extended_re:
-                count += find_pattern_by_line(item, pattern, args.ignore_case,
-                                              args.line_number)
+            if item.is_file():
+                count += find_pattern_by_line(item, pattern,
+                                              args.ignore_case,
+                                              args.line_number,
+                                              args.regex)
 
-    elif current_path.is_file() and args.extended_re:
-        count += find_pattern_by_line_regex(current_path, pattern,
-                                            args.ignore_case, args.line_number)
-    elif current_path.is_file() and not args.extended_re:
-        count += find_pattern_by_line(current_path, pattern, args.ignore_case,
-                                      args.line_number)
+    elif current_path.is_file():
+        count += find_pattern_by_line(current_path, pattern,
+                                      args.ignore_case, args.line_number,
+                                      args.regex)
 
     else:
         print(f"We dont know what it is, better dont touch.")
@@ -135,7 +132,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find pattern in files")
     parser.add_argument('file_path', type=Path, )  # positional argument
     parser.add_argument('pattern', type=str)  # positional argument
-    parser.add_argument('-E', '--extended_re', action='store_true')
+    parser.add_argument('-E', '--regex', action='store_true')
     parser.add_argument('-i', '--ignore_case', action='store_true')
     parser.add_argument('-c', '--count', action='store_true')
     parser.add_argument('-n', '--line_number', action='store_true')
