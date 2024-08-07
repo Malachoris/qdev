4545440e6094bc2e08d700ed56026be334cb3178
lukas.smicys <lukas.smicys@ext.fmc-ag.com>
2024-08-07 13:44:20 +0300

    Christmas

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def find_pattern_by_line_re(file: Path, pattern, ignore_case=False,
         return count
     for index, line in enumerate(lines):
         if not ignore_case:
-            match = re.search(pattern, line)
+            match = re.search(pattern, line) # "<re.Match object; span=(11, 17), match='simple'>"
             if match and not line_nr:
                 count += 1
                 print(line)
@@ -20,7 +20,7 @@ def find_pattern_by_line_re(file: Path, pattern, ignore_case=False,
                 count += 1
                 print(index, line)
         else:
-            match = re.search(pattern.lower(), line.lower())
+            match = re.search(pattern, line, flags=re.IGNORECASE)
             if match and not line_nr:
                 count += 1
                 print(line)
@@ -42,86 +42,86 @@ def find_pattern_by_line(file: Path, pattern, ignore_case=False,
             if pattern in line and not line_nr:
                 count += 1
                 print(line)
-            elif pattern and line_nr:
+            elif pattern in line and line_nr:
                 count += 1
                 print(index, line)
         else:
             if pattern.lower() in line.lower() and not line_nr:
                 count += 1
                 print(line)
-            elif pattern and line_nr:
+            elif pattern.lower() in line.lower() and line_nr:
                 count += 1
                 print(index, line)
     return count
 
 
-current_path = Path(".")
-pattern = "simple"  # "^def\s" "simple"
-recursive = False
-ignore_case = False
-line_number = False
-extended_re = False
-count_arg = False
+# current_path = Path(".")
+# pattern = "^def\s"  # "^def\s" "simple"
+# recursive = False
+# ignore_case = False
+# line_number = False
+# extended_re = True
+# count_arg = True
+#
+# count = 0
+#
+# search_level = current_path.glob("*")
+# if recursive:
+#     search_level = current_path.rglob("*")
+#
+# if ignore_case:
+#     pattern = pattern.lower()
+#
+# if current_path.is_dir():
+#     for item in search_level:
+#         if item.is_file() and extended_re:
+#             count += find_pattern_by_line_re(item, pattern, ignore_case,
+#                                              line_number)
+#         elif item.is_file() and not extended_re:
+#             count += find_pattern_by_line(item, pattern, ignore_case,
+#                                           line_number)
+#
+# elif current_path.is_file() and extended_re:
+#     count += find_pattern_by_line_re(current_path, pattern, ignore_case,
+#                                      line_number)
+# elif current_path.is_file() and not extended_re:
+#     count += find_pattern_by_line(current_path, pattern, ignore_case,
+#                                   line_number)
+#
+# else:
+#     print(f"We dont know what it is, better dont touch.")
+#
+# if count_arg:
+#     print(f"Number of rows where pattern repeats is {count}")
 
-count = 0
 
-search_level = current_path.glob("*")
-if recursive:
-    search_level = current_path.rglob("*")
-
-# if args.extended_re:
-#     pattern = args.extended_re
-
-if current_path.is_dir():
-    for item in search_level:
-        if item.is_file() and extended_re:
-            count += find_pattern_by_line_re(item, pattern, ignore_case,
-                                             line_number)
-        elif item.is_file() and not extended_re:
-            count += find_pattern_by_line(item, pattern, ignore_case,
-                                          line_number)
-
-elif current_path.is_file() and extended_re:
-    count += find_pattern_by_line_re(current_path, pattern, ignore_case,
-                                     line_number)
-elif current_path.is_file() and not extended_re:
-    count += find_pattern_by_line(current_path, pattern, ignore_case,
-                                  line_number)
-
-else:
-    print(f"We dont know what it is, better dont touch.")
-
-if count_arg:
-    print(f"Number of rows where pattern repeats is {count}")
-
-
-def search_for_pattern(arg: argparse.Namespace):
-    current_path = arg.file_path
-    pattern = arg.pattern
+def search_for_pattern(args: argparse.Namespace):
+    current_path = args.file_path
+    pattern = args.pattern
     count = 0
 
-    search_level = current_path.glob("*")
+    searched_files = current_path.glob("*")
     if args.recursive:
-        search_level = current_path.rglob("*")
+        searched_files = current_path.rglob("*")
 
     # if args.extended_re:
     #     pattern = args.extended_re
 
     if current_path.is_dir():
-        for item in search_level:
+        for item in searched_files:
             if item.is_file() and args.extended_re:
                 count += find_pattern_by_line_re(item, pattern,
-                                                 arg.ignore_case,
+                                                 args.ignore_case,
                                                  args.line_number)
             elif item.is_file() and not args.extended_re:
-                count += find_pattern_by_line(item, pattern, arg.ignore_case,
+                count += find_pattern_by_line(item, pattern, args.ignore_case,
                                               args.line_number)
 
     elif current_path.is_file() and args.extended_re:
         count += find_pattern_by_line_re(current_path, pattern,
-                                         arg.ignore_case, args.line_number)
+                                         args.ignore_case, args.line_number)
     elif current_path.is_file() and not args.extended_re:
-        count += find_pattern_by_line(current_path, pattern, arg.ignore_case,
+        count += find_pattern_by_line(current_path, pattern, args.ignore_case,
                                       args.line_number)
 
     else:
@@ -129,7 +129,7 @@ def search_for_pattern(arg: argparse.Namespace):
         return
 
     if args.count:
-        print(count)
+        print(f"Number of rows where pattern repeats is {count}")
 
 
 if __name__ == "__main__":
@@ -149,5 +149,5 @@ if __name__ == "__main__":
     # parser.add_argument('--include')
 
     args = parser.parse_args()
-    print(args.file_path, args.pattern)
+    # print(args.file_path, args.pattern)
     search_for_pattern(args)
